fc4bb4dfd505cfeefff0cb81fc436a4f73170b71
sekinat95 <sekinat95@gmail.com>
2021-03-15 13:07:52 +0100

    class methods and static methods

--- oop.py
+++ oop.py
@@ -11,12 +11,29 @@ print(type(obj1))
 
 
 class Player:
+    # class object attribiute (static)
+    membership = True
+
     def __init__(self, name, age):
-        self.name = name  # attributes
-        self.age = age
+        # class attributes (dynamic)
+        if (Player.membership):
+            self.name = name
+            self.age = age
 
     def run(self):
         print('run')
+
+    def shoutout(self):
+        print(f"my name is {self.name}")
+
+    # class methods and static methods
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls('teddy', num1+num2)
+
+    @staticmethod
+    def adding_things(cls, num1, num2):
+        return cls('teddy', num1+num2)
 
 
 player1 = Player('cydni', 23)
@@ -24,3 +41,6 @@ player2 = Player('ope', 45)
 
 print(player1.name)  # how to access your attributes
 print(player2.name)
+player1.shoutout()
+
+print(Player.adding_things(3, 7))
