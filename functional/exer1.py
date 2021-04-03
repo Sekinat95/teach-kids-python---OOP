a8a3144665e577c2506f116a699b01491cfbfec2
sekinat95 <sekinat95@gmail.com>
2021-04-03 16:32:47 +0100

    added comments to func prog and lambda exer

--- functional/exer1.py
+++ functional/exer1.py
@@ -3,10 +3,22 @@ from functools import reduce
 # 1 Capitalize all of the pet names and print the list
 my_pets = ['sisi', 'bibi', 'titi', 'carla']
 
+# name = "geeks FOR geeks"
+# print(name.capitalize())
+
+print(list(map(lambda x: x.capitalize(), my_pets)))
 
 # 2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
 my_strings = ['a', 'b', 'c', 'd', 'e']
 my_numbers = [5, 4, 3, 2, 1]
+
+print(list(zip(my_strings, my_numbers)))
+new_list = list(zip(my_strings, my_numbers))
+
+new_list.sort(key=lambda x: x[1])
+# remember sort takes two arguments: reverse(a true or false value), key(a fucntion). How do I know this? google ofcourse
+
+print(new_list)
 
 
 # 3 Filter the scores that pass over 50%
--- functional/exer2.py
+++ functional/exer2.py
@@ -5,4 +5,13 @@ print(list(map(lambda num: num**2, [1, 2, 3])))
 a = [(0, 2), (5, 2), (9, 9), (10, -1)]
 a.sort(key=lambda x: x[1])
 
+# explanation:
+"""
+list.sort() takes in an argument key , a fucntion that descrobes the sorting criteria
+remember lambda functions are anonymous functions that you intend to use only once. this
+makes them fit in here for usage. 
+so, in the solution, the sorting criteria indicates that the list should be sorted with each item of the second index in each tuple, 
+x represents each item in each tuple.
+"""
+
 print(a)
